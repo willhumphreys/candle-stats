f80d22667d0c277054ecaa28ff06feb3648875d9
Will Humphreys <will.humphreys@bbc.co.uk>
2016-02-19 18:32:55 +0000

    Start looking at whether the market tends to take out the nearest high/low

--- candle_stats.rb
+++ candle_stats.rb
@@ -4,25 +4,39 @@ file = File.read('EURGBP.json')
 data_hash = JSON.parse(file)
 quotes = data_hash['query']['results']['quote']
 
+quote_count = 0
 down_day_counter = 0
 next_day_down_counter = 0
+
+next_day_opens_in_range_count = 0
 
 def is_a_down_day(quote)
   quote['Close'] < quote['Open']
 end
 
-quotes.each_cons(2) do |first, second|
+quotes.each_cons(2) do |second, first|
+  quote_count += 1
+
   if is_a_down_day(first)
     down_day_counter +=1
     if is_a_down_day(second)
       next_day_down_counter +=1
     end
   end
-  puts "a #{first['Open']} b #{second['Open']}"
+
+  if second['Open'] > first['Low'] && second['Open'] < first['High']
+    next_day_opens_in_range_count += 1
+  else
+    puts 'This shouldn\'t happen often'
+  end
+
 end
 
 
 #If today is down day what are the odds the next day is a down day?
 today_down_tomorrow_down = (next_day_down_counter.to_f / down_day_counter * 100).ceil
+puts "If today is down what are the odds tomorrow is down to: #{today_down_tomorrow_down}%"
 
-puts "#{today_down_tomorrow_down}%"
+#Does today open inside the range of the previous day
+next_day_opens_in_range = (next_day_opens_in_range_count.to_f / quote_count * 100).ceil
+puts "Next day opens in range: #{next_day_opens_in_range}%"
