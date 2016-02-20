2b9ea4f09d6d0204e592fbe951a8ba26947c0bbd
Will Humphreys <will.humphreys@bbc.co.uk>
2016-02-20 11:42:15 +0000

    Remove some empty lines.

--- candle_stats.rb
+++ candle_stats.rb
@@ -8,28 +8,17 @@ file = File.read('EURGBP.json')
 data_hash = JSON.parse(file)
 quotes = data_hash['results']
 
-
-
-
-
 $candle_operations = CandleOperations.new
 $quote_counter = QuoteCounter.new
 $nearest_take_out = NearestTakeOut.new
-
 $down_days = DownDays.new
 
 quotes.each_cons(2) do |first, second|
   $quote_counter.process(first,second)
-
-
   $down_days.process(first, second)
   $nearest_take_out.process(first, second)
-
-
 end
 
-
 $down_days.display
-
 $nearest_take_out.display($quote_counter.count)
 
