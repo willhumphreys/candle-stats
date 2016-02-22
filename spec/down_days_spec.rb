d5bdf5765e3f6e1a50eeae916e5be0e782e4b1cb
Will Humphreys <will.humphreys@bbc.co.uk>
2016-02-22 10:53:59 +0000

    Pull out the reused setup code into the before.

--- spec/down_days_spec.rb
+++ spec/down_days_spec.rb
@@ -5,18 +5,17 @@ describe 'My behaviour' do
 
   before do
     @down_days = DownDays.new
+    # noinspection RubyStringKeysInHashInspection
+    @first_quote = {'open' => 10, 'close' => 9}
+    # noinspection RubyStringKeysInHashInspection
+    @second_quote = { 'open' => 9, 'close' => 8}
   end
 
   it 'should increment the sequential counter if the two supplied quotes are down' do
-    # noinspection RubyStringKeysInHashInspection
-    first_quote = {'open' => 10, 'close' => 9}
-    # noinspection RubyStringKeysInHashInspection
-    second_quote = { 'open' => 9, 'close' => 8}
-
     expect(@down_days.get_both_ticks_count).to equal(0)
     expect(@down_days.get_first_tick_count).to equal(0)
 
-    @down_days.process(first_quote, second_quote)
+    @down_days.process(@first_quote, @second_quote)
 
     expect(@down_days.get_both_ticks_count).to equal(1)
     expect(@down_days.get_first_tick_count).to equal(1)
@@ -24,16 +23,17 @@ describe 'My behaviour' do
 
   it 'should not increment the counter if the first quote is down but the second quote is up' do
     # noinspection RubyStringKeysInHashInspection
-    first_quote = {'open' => 10, 'close' => 9}
-    # noinspection RubyStringKeysInHashInspection
-    second_quote = { 'open' => 9, 'close' => 10}
+    @second_quote = { 'open' => 9, 'close' => 10}
 
     expect(@down_days.get_both_ticks_count).to equal(0)
     expect(@down_days.get_first_tick_count).to equal(0)
 
-    @down_days.process(first_quote, second_quote)
+    @down_days.process(@first_quote, @second_quote)
 
     expect(@down_days.get_both_ticks_count).to equal(0)
     expect(@down_days.get_first_tick_count).to equal(1)
   end
+
+
+
 end
