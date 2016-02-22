379d1de1110e302485159a511b6dc12a9bdcdd52
Will Humphreys <will.humphreys@bbc.co.uk>
2016-02-22 10:45:40 +0000

    Rename fields to make it things clearer.

--- down_days.rb
+++ down_days.rb
@@ -3,27 +3,30 @@ require_relative 'candle_operations'
 class DownDays
   def initialize
     @candle_operations = CandleOperations.new
-    @down_day_counter = 0
-    @sequential_down_day_counter = 0
+    @first_tick_count = 0
+    @both_ticks_count = 0
   end
 
   def process(first, second)
     if @candle_operations.is_a_down_day(first)
-      @down_day_counter +=1
+      @first_tick_count +=1
       if @candle_operations.is_a_down_day(second)
-        @sequential_down_day_counter +=1
+        @both_ticks_count +=1
       end
     end
   end
 
-  def count
-    @sequential_down_day_counter
+  def get_both_ticks_count
+    @both_ticks_count
+  end
+
+  def get_first_tick_count
+    @first_tick_count
   end
 
   def display
     #If today is down day what are the odds the next day is a down day?
-    today_down_tomorrow_down = (@sequential_down_day_counter.to_f / @down_day_counter * 100).ceil
+    today_down_tomorrow_down = (@both_ticks_count.to_f / @first_tick_count * 100).ceil
     puts "If today is down what are the odds tomorrow is down to: #{today_down_tomorrow_down}%"
   end
-
 end
--- spec/down_days_spec.rb
+++ spec/down_days_spec.rb
@@ -7,17 +7,33 @@ describe 'My behaviour' do
     @down_days = DownDays.new
   end
 
-  it 'should increment the counter if the two supplied quotes are down' do
-
+  it 'should increment the sequential counter if the two supplied quotes are down' do
     # noinspection RubyStringKeysInHashInspection
     first_quote = {'open' => 10, 'close' => 9}
     # noinspection RubyStringKeysInHashInspection
     second_quote = { 'open' => 9, 'close' => 8}
 
-    expect(@down_days.count).to equal(0)
+    expect(@down_days.get_both_ticks_count).to equal(0)
+    expect(@down_days.get_first_tick_count).to equal(0)
 
     @down_days.process(first_quote, second_quote)
 
-    expect(@down_days.count).to equal(1)
+    expect(@down_days.get_both_ticks_count).to equal(1)
+    expect(@down_days.get_first_tick_count).to equal(1)
+  end
+
+  it 'should not increment the counter if the first quote is down but the second quote is up' do
+    # noinspection RubyStringKeysInHashInspection
+    first_quote = {'open' => 10, 'close' => 9}
+    # noinspection RubyStringKeysInHashInspection
+    second_quote = { 'open' => 9, 'close' => 10}
+
+    expect(@down_days.get_both_ticks_count).to equal(0)
+    expect(@down_days.get_first_tick_count).to equal(0)
+
+    @down_days.process(first_quote, second_quote)
+
+    expect(@down_days.get_both_ticks_count).to equal(0)
+    expect(@down_days.get_first_tick_count).to equal(1)
   end
 end
