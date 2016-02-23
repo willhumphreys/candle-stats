d9808e7fef81b63da41f562744fd1de0da851872
Will Humphreys <will.humphreys@bbc.co.uk>
2016-02-23 14:59:52 +0000

    Rename gap fields

--- do_gaps_close.rb
+++ do_gaps_close.rb
@@ -3,8 +3,8 @@ require_relative 'candle_operations'
 class DoGapsClose
   def initialize
     @candle_operations = CandleOperations.new
-    @gap_count = 0
-    @gap_closed_count = 0
+    @full_candle_gap_count = 0
+    @full_candle_gap_closed_count = 0
     @candle_count = 0
   end
 
@@ -12,24 +12,24 @@ class DoGapsClose
     @candle_count += 1
 
     if @candle_operations.full_candle_gaps(first, second)
-      @gap_count += 1
+      @full_candle_gap_count += 1
       if @candle_operations.full_candle_gap_closes(first, second)
-        @gap_closed_count += 1
+        @full_candle_gap_closed_count += 1
       end
     end
   end
 
   def get_gap_count
-    @gap_count
+    @full_candle_gap_count
   end
 
   def gap_closed_count
-    @gap_closed_count
+    @full_candle_gap_closed_count
   end
 
   def display
-    puts "Total days #{@candle_count}. Total full candle gap count #{@gap_count}."\
-    " Closed full candle gaps #{@gap_closed_count}"
+    puts "Total days #{@candle_count}. Total full candle gap count #{@full_candle_gap_count}."\
+    " Closed full candle gaps #{@full_candle_gap_closed_count}"
   end
 
 end
