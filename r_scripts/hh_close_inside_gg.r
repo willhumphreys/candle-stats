4cc41ee52743f71acbb31fb23bc5c0d1d1f6cd82
Will Humphreys <whumphreys@gmail.com>
2016-07-10 10:49:12 +0100

    Add lines to show when 20 50 and 80 percent of the distance.

--- r_scripts/hh_close_inside_gg.r
+++ r_scripts/hh_close_inside_gg.r
@@ -13,7 +13,14 @@ data$pc.50 = predict(fit.50)
 data$pc.20 = predict(fit.20)
 data$pc.80 = predict(fit.80)
 higher_high_close_in_range_plot <- ggplot(data, aes(x = date.time)) +
-geom_point(aes(y = pips)) + geom_line(aes(y = pc.50, colour = 'Median')) +
-geom_line(aes(y = pc.80, colour = '80')) + geom_line(aes(y = pc.20, colour = '20')) +
-theme_solarized() + ggtitle("Break size where we close back inside")
+geom_point(aes(y = pips)) +
+geom_line(aes(y = pc.50, colour = 'Median')) +
+geom_line(aes(y = pc.80, colour = '80')) +
+geom_line(aes(y = pc.20, colour = '20')) +
+geom_hline(aes(yintercept = quantile(data$pips, .50), colour = 'Median')) +
+geom_hline(aes(yintercept = quantile(data$pips, .80), colour = '80')) +
+geom_hline(aes(yintercept = quantile(data$pips, .20), colour = '20')) +
+labs(x="Date",y="Pips") +
+theme_solarized() +
+ggtitle("Break size when we close back inside")
 ggsave(file="out/higher_high_close_in_range.png")
