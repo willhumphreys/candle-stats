1c0801c25f9a474d9888e030fb4e5bb72bc350d8
Will Humphreys <whumphreys@gmail.com>
2016-07-10 11:53:05 +0100

    Pull out the quantiles. Tidyup the names. Fix the title.

--- r_scripts/hh_close_inside_gg.r
+++ r_scripts/hh_close_inside_gg.r
@@ -1,7 +1,9 @@
+library(SparseM)
 library(ggplot2)
 library(quantreg)
 library(plyr)
 library(ggthemes)
+
 data <- read.table("out/higher_high_close_in_range.csv", header=T,sep=",")
 data$fixed.date.time=as.POSIXct(data$date.time, tz = "UTC")
 data$date.time <- data$fixed.date.time
@@ -12,15 +14,26 @@ fit.20 <- quantreg::rq(pips ~ splines::bs(date.time, df=15), tau=0.20, data=data
 data$pc.50 = predict(fit.50)
 data$pc.20 = predict(fit.20)
 data$pc.80 = predict(fit.80)
-higher_high_close_in_range_plot <- ggplot(data, aes(x = date.time)) +
+
+q20 <- quantile(data$pips, .20)
+q50 <- quantile(data$pips, .50)
+q80 <- quantile(data$pips, .80)
+
+plot <- ggplot(data, aes(x = date.time)) +
 geom_point(aes(y = pips)) +
+
+geom_line(aes(y = pc.80, colour = '80th percentile')) +
+geom_line(aes(y = pc.20, colour = '20th percentile')) +
 geom_line(aes(y = pc.50, colour = 'Median')) +
-geom_line(aes(y = pc.80, colour = '80')) +
-geom_line(aes(y = pc.20, colour = '20')) +
-geom_hline(aes(yintercept = quantile(data$pips, .50), colour = 'Median')) +
-geom_hline(aes(yintercept = quantile(data$pips, .80), colour = '80')) +
-geom_hline(aes(yintercept = quantile(data$pips, .20), colour = '20')) +
+geom_hline(aes(yintercept = q20, colour = '20th percentile')) +
+geom_hline(aes(yintercept = q50, colour = 'Median')) +
+geom_hline(aes(yintercept = q80, colour = '80th percentile')) +
+
+scale_y_continuous(breaks=seq(0, 250, 10))  +
+guides(fill=guide_legend(title=NULL)) +
 labs(x="Date",y="Pips") +
 theme_solarized() +
-ggtitle("Break size when we close back inside")
+ggtitle("Break size when we close back inside AUDUSD Weekly") +
+theme(legend.title=element_blank())
+
 ggsave(file="out/higher_high_close_in_range.png")
