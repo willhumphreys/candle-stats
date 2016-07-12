af3bda46b3789772090c3072e22cfd525b533389
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-12 15:50:38 +0100

    Remove old files

--- quote.rb
+++ quote.rb
@@ -1,15 +1,5 @@
 class Quote
 
-  # "symbol": "^EURGBP",
-  #     "timestamp": "2015-02-19T00:00:00-06:00",
-  #     "tradingDay": "2015-02-19",
-  #     "open": 0.7381,
-  #     "high": 0.74086,
-  #     "low": 0.73574,
-  #     "close": 0.73735,
-  #     "volume": 277112,
-  #     "openInterest": null
-
   def initialize(symbol: , timestamp: , trading_day: , open: , high: , low: , close: , volume: , open_interest: )
 
     @symbol = symbol
