b3e16c533f9a173a8e965789203d03cd3931bd07
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-12 15:32:25 +0100

    Delete old requires.

--- candle_stats.rb
+++ candle_stats.rb
@@ -1,21 +1,8 @@
 require 'json'
-require_relative 'quote_counter'
-require_relative 'down_days'
-require_relative 'up_days'
-require_relative 'nearest_take_out'
-require_relative 'do_full_candle_gaps_close'
-require_relative 'candle_gaps_close'
-require_relative 'days_close_same_direction'
-require_relative 'take_out_wrong_end'
-require_relative 'higher_high_and_lower_low'
-require_relative 'higher_high_and_higher_low'
-require_relative 'lower_high_and_lower_low'
-require_relative 'inside_day_and_inside_day'
-
 require_relative 'bar_chart_file_repo'
 require_relative 'mt4_file_repo'
 
-require_relative 'take_outs'
+
 require_relative 'higher_high_close_above'
 require_relative 'higher_high_close_inside'
 
