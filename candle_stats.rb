60d5999fcf85cdc2cf455e5aa5dd57b3ef96ea99
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-13 11:05:46 +0100

    Pull out the processors to a separate class to start getting the code back to being testable.

--- candle_stats.rb
+++ candle_stats.rb
@@ -4,38 +4,12 @@ require_relative 'mt4_file_repo'
 require_relative 'stat_executor'
 require_relative 'candle_operations'
 require_relative 'processor'
+require_relative 'processors'
 
 @bar_chart_file_repo = BarChartFileRepo.new
 @mt4_file_repo = MT4FileRepo.new
 @candle_ops = CandleOperations.new
-
-higher_high_close_above_l = lambda do |first, second|
-  if @candle_ops.is_a_higher_high_in(first, second) && @candle_ops.closes_above_range(first, second)
-    return ((second.high - first.high) * 10000).round(0)
-  end
-  return nil
-end
-
-higher_high_close_inside_l = lambda do |first, second|
-  if @candle_ops.is_a_higher_high_in(first, second) && @candle_ops.closes_inside_range(first, second)
-    return ((second.high - first.high) * 10000).round(0)
-  end
-  nil
-end
-
-lower_low_close_below_l = lambda do |first, second|
-  if @candle_ops.is_a_lower_low_in(first, second) && @candle_ops.closes_below_range(first, second)
-    return ((second.low - first.low) * 10000).round(0)
-  end
-  return nil
-end
-
-lower_low_close_inside_l = lambda do |first, second|
-  if @candle_ops.is_a_lower_low_in(first, second) && @candle_ops.closes_inside_range(first, second)
-    return ((second.low - first.low) * 10000).round(0)
-  end
-  nil
-end
+@processors = Processors.new
 
 time_periods = ['10080']
 symbols = ['AUDUSD']
@@ -45,11 +19,7 @@ data_sets = symbols.product(time_periods).collect { |time_period, symbol| time_p
 data_sets.each { |data_set|
   quotes = @mt4_file_repo.read_quotes("data/#{data_set}.csv")
 
-  executors = [Stat_Executor.new(data_set, Processor.new('higher_high_close_above', higher_high_close_above_l)),
-               Stat_Executor.new(data_set, Processor.new('higher_high_close_inside', higher_high_close_inside_l)),
-               Stat_Executor.new(data_set, Processor.new('lower_low_close_above', lower_low_close_below_l)),
-               Stat_Executor.new(data_set, Processor.new('lower_low_close_inside', lower_low_close_inside_l))]
-
+  executors = @processors.processors.collect { |processor| Stat_Executor.new(data_set, processor) }
 
   quotes.each_cons(6) do |first, second, third, fourth, fifth, sixth|
 
---
+++ processors.rb
@@ -0,0 +1,43 @@
+require_relative 'candle_operations'
+class Processors
+  attr_reader :processors
+
+  def initialize
+    @candle_ops = CandleOperations.new
+
+    higher_high_close_above_l = lambda do |first, second|
+      if @candle_ops.is_a_higher_high_in(first, second) && @candle_ops.closes_above_range(first, second)
+        return ((second.high - first.high) * 10000).round(0)
+      end
+      return nil
+    end
+
+    higher_high_close_inside_l = lambda do |first, second|
+      if @candle_ops.is_a_higher_high_in(first, second) && @candle_ops.closes_inside_range(first, second)
+        return ((second.high - first.high) * 10000).round(0)
+      end
+      nil
+    end
+
+    lower_low_close_below_l = lambda do |first, second|
+      if @candle_ops.is_a_lower_low_in(first, second) && @candle_ops.closes_below_range(first, second)
+        return ((second.low - first.low) * 10000).round(0)
+      end
+      return nil
+    end
+
+    lower_low_close_inside_l = lambda do |first, second|
+      if @candle_ops.is_a_lower_low_in(first, second) && @candle_ops.closes_inside_range(first, second)
+        return ((second.low - first.low) * 10000).round(0)
+      end
+      nil
+    end
+
+    @processors = [
+        Processor.new('higher_high_close_above', higher_high_close_above_l),
+        Processor.new('higher_high_close_inside', higher_high_close_inside_l),
+        Processor.new('lower_low_close_above', lower_low_close_below_l),
+        Processor.new('lower_low_close_inside', lower_low_close_inside_l)
+    ]
+  end
+end
