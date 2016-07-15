de478e64b70ae6ff997a39ae72b1cc422f2d44e5
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-15 11:11:59 +0100

    Completed the quantile graphs.

--- r_scripts/quantile_graph.r
+++ r_scripts/quantile_graph.r
@@ -7,22 +7,30 @@ library(ggthemes)
 timeframes <- c("1440", "10080")
 names <- c("higher_high_close_inside", "higher_high_close_above", "lower_low_close_below", "lower_low_close_inside")
 
+quantiles <- c("P20", "P50", "P80")
+
 name_timeframe_combinations <- expand.grid(timeframes,names)
 
 combinations <- paste(name_timeframe_combinations$Var1, name_timeframe_combinations$Var2, sep="_")
 
-generate.quantile.plot <- function(name, data) {
-    higher_high_close_inside <- data[grep(name, data$symbol), ]
-    higher_high_close_inside$symbol = strtrim(higher_high_close_inside$symbol, 6)
+generate.quantile.plot <- function(combination, data) {
+    fixed_data <- data[grep(combination, data$symbol), ]
+    fixed_data$symbol = strtrim(fixed_data$symbol, 6)
 
-    ggplot(data=higher_high_close_inside, aes(x=symbol, y=P20)) +
+    sapply(quantiles, function(quantile) plot.quantiles(fixed_data, combination, quantile))
+}
+
+plot.quantiles <- function(data, name, quantile) {
+    ggplot(data=data, aes_string(x="symbol", y=quantile)) +
         geom_bar(colour="black", fill="#DD8888", width=.8, stat="identity") +
         theme_solarized() +
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-    ggsave(file=paste("plots/", name, ".png", sep = ""))
+    graph_name <- paste("plots/", name, quantile, ".png", sep = "")
+    cat(graph_name)
+    ggsave(file= graph_name)
 }
 
 data <- read.table('r_out/quantiles.csv', header=T,sep=",")
 
-sapply(combinations, function(x) generate.quantile.plot(x, data))
+sapply(combinations, function(combination) generate.quantile.plot(combination, data))
