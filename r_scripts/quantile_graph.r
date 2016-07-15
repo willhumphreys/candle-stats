299ded9fc14d3fb12526578a27564f18735fd20d
Will Humphreys <will.humphreys@bbc.co.uk>
2016-07-15 10:07:15 +0100

    Add quantile graph code.

--- r_scripts/quantile_graph.r
+++ r_scripts/quantile_graph.r
@@ -4,11 +4,26 @@ library(quantreg)
 library(plyr)
 library(ggthemes)
 
+timeframes <- c("1440", "10080")
+names <- c("higher_high_close_inside", "higher_high_close_above", "lower_low_close_below", "lower_low_close_inside")
+
+name_timeframe_combinations <- expand.grid(timeframes,names)
+
+combinations <- paste(name_timeframe_combinations$Var1, name_timeframe_combinations$Var2, sep="_")
+
+generate.quantile.plot <- function(name, data) {
+    higher_high_close_inside <- data[grep(name, data$symbol), ]
+    higher_high_close_inside$symbol = strtrim(higher_high_close_inside$symbol, 6)
+
+    ggplot(data=higher_high_close_inside, aes(x=symbol, y=P20)) +
+        geom_bar(colour="black", fill="#DD8888", width=.8, stat="identity") +
+        theme_solarized() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+    ggsave(file=paste("plots/", name, ".png", sep = ""))
+
+}
+
 data <- read.table('r_out/quantiles.csv', header=T,sep=",")
 
-ggplot(data, aes(x = symbol, y = X20))
-
-higher_high_close_inside <- data[grep("higher_high_close_inside", data$symbol), ]
-
-ggplot(data=higher_high_close_inside, aes(x=symbol, y=P20)) +
-    geom_bar(stat="identity")
+sapply(combinations, function(x) generate.quantile.plot(x, data))
