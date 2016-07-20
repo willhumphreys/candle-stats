fa705488255539d78a93bd4315925bb0bc8d805c
Will Humphreys <whumphreys@gmail.com>
2016-07-20 20:21:14 +0100

    Start adding news data. EURUSD for now.

--- candle_stats.rb
+++ candle_stats.rb
@@ -5,6 +5,7 @@ require_relative 'stat_executor'
 require_relative 'candle_operations'
 require_relative 'processor'
 require_relative 'processors'
+require_relative 'news_reader'
 
 @bar_chart_file_repo = BarChartFileRepo.new
 @mt4_file_repo = MT4FileRepo.new
@@ -12,7 +13,7 @@ require_relative 'processors'
 @processors = Processors.new
 
 time_periods = %w(10080 1440)
-symbols = %w(AUDUSD EURCHF EURGBP EURJPY EURUSD GBPUSD USDCAD USDCHF USDJPY)
+symbols = %w(AUDUSD EURCHF EURGBP EURJPY EURUSD GBPUSD USDCAD USDCHF USDJPY NZDUSD)
 
 data_sets = symbols.product(time_periods).collect { |time_period, symbol| time_period + symbol }
 
