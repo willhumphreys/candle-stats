ab54f4aa2351a3d3cb4b90012125fa8c05bf138d
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-07 17:05:43 +0100

    Add the option to just look at last year.

--- r_scripts/consecutive_wins.r
+++ r_scripts/consecutive_wins.r
@@ -4,10 +4,19 @@ generate.plot <- function(file.in) {
     file.out <- paste("consecutive_plots/",(strsplit(file.in, split='.', fixed=TRUE)[[1]])[1], ".png", sep = "")
 
     data <- read.table(paste("consecutive_out/",file.in, sep=""), header=T,sep=",")
-    data$date.time=as.POSIXct(data$date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S")
+
+    #data <- data[-2,]
+    data <- data[complete.cases(data),]
+
+    data$date.time=as.Date(as.POSIXct(data$date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S"))
     data$date <- NULL
 
-    ggplot(data=data, aes(x=date.time, y=pips, group = 1)) +
+    fromDate <- as.Date("2015-08-07")
+    toDate <- as.Date("2016-08-07")
+
+    last12Months <- data[data$date.time >= fromDate & data$date.time <= toDate,]
+
+    ggplot(data=last12Months, aes(x=date.time, y=pips, group = 1)) +
     geom_line() +
     geom_line(aes(y = 50, colour = 'RED')) +
     ggtitle(file.in)
