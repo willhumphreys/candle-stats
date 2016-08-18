b9b1eab4412c7a55307e2512caab1a23c76d8598
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-18 13:46:41 +0100

    Add all the ruby code to support varying time periods.

--- fade_the_highs.rb
+++ fade_the_highs.rb
@@ -7,11 +7,13 @@ require_relative 'processor'
 require_relative 'processors'
 require_relative 'news_reader'
 require_relative 'fade_mapper'
+require 'active_support/all'
 
 @bar_chart_file_repo = BarChartFileRepo.new
 @mt4_file_repo = MT4FileRepo.new(FadeMapper.new)
 @candle_ops = CandleOperations.new
 @processors = Processors.new
+
 
 def reset_fields
 
@@ -44,28 +46,31 @@ symbols = %w(AUDUSD EURCHF EURGBP EURUSD GBPUSD USDCAD USDCHF NZDUSD)
 
 data_sets = symbols.product(time_periods).collect { |time_period, symbol| time_period + symbol }
 
-File.delete('out.csv') if File.exist?('out.csv')
-open('out.csv', 'a') { |f|
-  f << "scenario, data_set, consecutive, fails_or_wins, result\n"
-}
 
-def log_csv(data_set, title)
-  open('out.csv', 'a') { |f|
-    f << "#{title},#{data_set},profit 1 (#{@profit_1}) - odds 2 profit (#{@profit_1_2}), wins,#{((@profit_1_2 / @profit_1) * 100).round(2)}\n"
-    f << "#{title},#{data_set},profit 1-2 (#{@profit_1_2}) - odds 3 profit (#{@profit_1_2_3}), wins,#{((@profit_1_2_3 / @profit_1_2) * 100).round(2)}\n"
-    f << "#{title},#{data_set},profit 1-2-3 (#{@profit_1_2_3}) - odds 4 profit (#{@profit_1_2_3_4}), wins,#{((@profit_1_2_3_4 / @profit_1_2_3) * 100).round(2)}\n"
-    f << "#{title},#{data_set},profit 1-2-3-4 (#{@profit_1_2_3_4}) - odds 5 profit (#{@profit_1_2_3_4_5}), wins,#{((@profit_1_2_3_4_5 / @profit_1_2_3_4) * 100).round(2)}\n"
 
-    f << "#{title},#{data_set},loss 1 (#{@loss_1}) - odds 2 loss (#{@loss_1_2}), losses,#{((@loss_1_2 / @loss_1) * 100).round(2)}\n"
-    f << "#{title},#{data_set},loss 1-2 (#{@loss_1_2}) - odds 3 loss (#{@loss_1_2_3}), losses,#{((@loss_1_2_3 / @loss_1_2) * 100).round(2)}\n"
-    f << "#{title},#{data_set},loss 1-2-3 (#{@loss_1_2_3}) - odds 4 loss (#{@loss_1_2_3_4}), losses,#{((@loss_1_2_3_4 / @loss_1_2_3) * 100).round(2)}\n"
-    f << "#{title},#{data_set},loss 1-2-3-4 (#{@loss_1_2_3_4}) - odds 5 loss (#{@loss_1_2_3_4_5}), losses,#{((@loss_1_2_3_4_5 / @loss_1_2_3_4) * 100).round(2)}\n"
+def log_csv(data_set, title, file_out)
+  open(file_out, 'a') { |f|
+    f << "#{title},#{data_set},profit 1-2,wins,#{((@profit_1_2 / @profit_1) * 100).round(2)}\n"
+    f << "#{title},#{data_set},profit 1-2-3,wins,#{((@profit_1_2_3 / @profit_1_2) * 100).round(2)}\n"
+    f << "#{title},#{data_set},profit 1-2-3-4,wins,#{((@profit_1_2_3_4 / @profit_1_2_3) * 100).round(2)}\n"
+    f << "#{title},#{data_set},profit 1-2-3-4-5,wins,#{((@profit_1_2_3_4_5 / @profit_1_2_3_4) * 100).round(2)}\n"
+
+    f << "#{title},#{data_set},loss 1-2,losses,#{((@loss_1_2 / @loss_1) * 100).round(2)}\n"
+    f << "#{title},#{data_set},loss 1-2-3,losses,#{((@loss_1_2_3 / @loss_1_2) * 100).round(2)}\n"
+    f << "#{title},#{data_set},loss 1-2-3-4,losses,#{((@loss_1_2_3_4 / @loss_1_2_3) * 100).round(2)}\n"
+    f << "#{title},#{data_set},loss 1-2-3-4-5,losses,#{((@loss_1_2_3_4_5 / @loss_1_2_3_4) * 100).round(2)}\n"
   }
 
 end
 
-def process(data_set, profits, title)
+def process(data_set, profits, title, start_date, end_date, file_out)
   profits.each_cons(6) do |first, second, third, fourth, fifth, sixth|
+
+    if first.timestamp.utc < start_date || first.timestamp.utc > end_date
+      next
+    end
+
+
     if first.profit > 0
       @profit_1 += 1
       if second.profit > 0
@@ -98,9 +103,10 @@ def process(data_set, profits, title)
       end
     end
   end
+  #end
 
 
-  log_csv(data_set, title)
+  log_csv(data_set, title, file_out)
 
   reset_fields
 
@@ -118,9 +124,25 @@ data_sets.each { |data_set|
   end
 
 
-  process(data_set, profits, 'both')
-  process(data_set, fail_at_highs, 'fail at highs')
-  process(data_set, fail_at_lows, 'fail at lows')
+
+
+  8.times do |count|
+    start_date = DateTime.now - 12.months - (12 * count).months
+    end_date = DateTime.now - (12 * count).months
+
+    file_out = "out-#{start_date}-#{end_date}.csv}"
+    File.delete(file_out) if File.exist?(file_out)
+    open(file_out, 'a') { |f|
+      f << "scenario, data_set, consecutive, fails_or_wins, result\n"
+    }
+
+    # process(data_set, profits, 'both')
+    process(data_set, fail_at_highs, 'fail at highs', start_date, end_date, file_out)
+    process(data_set, fail_at_lows, 'fail at lows', start_date, end_date, file_out)
+
+  end
+
+
 }
 
 puts 'done'
--- r_scripts/fade_the_highs_plots.r
+++ r_scripts/fade_the_highs_plots.r
@@ -1,7 +1,20 @@
 library(ggplot2)
 data <- read.table('out.csv', header=T,sep=",")
-data$plot.name <- paste(data$scenario,'-',data$data_set,'-',data$consecutive, '-', data$fails_or_wins)
-ggplot(data=data, aes(x=plot.name, y=result, fill=scenario)) +geom_bar(stat="identity")
 
-audusd <- subset(data, data_set == "AUDUSD_FadeTheBreakoutNormal")
-ggplot(data=audusd, aes(x=plot.name, y=result, fill=scenario)) +geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+generate.plot <- function(symbol, data) {
+  data_for_symbol <- subset(data, data_set == symbol)
+
+  ggplot(data=data_for_symbol, aes(x=consecutive, y=result, fill=scenario)) +geom_bar(stat="identity", position=position_dodge()) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  ggsave(file=paste('fade_highs_low_graphs/', symbol, ".png", sep = ""))
+}
+
+
+scenarios <- c("FadeTheBreakoutNormal")
+symbols <- c("AUDUSD", "EURCHF", "EURGBP", "EURUSD", "GBPUSD", "NZDUSD", "USDCHF")
+
+
+scenario_symbols <- expand.grid(symbols, scenarios)
+
+combinations <- paste(scenario_symbols$Var1, scenario_symbols$Var2, sep="_")
+
+sapply(combinations, function(combination) generate.plot(combination, data))
