b3850dd8228bf0201d6a69b5453c423c74aeed4d
Will Humphreys <will.humphreys@bbc.co.uk>
2016-08-22 15:49:02 +0100

    Tidyup the graph.

--- r_scripts/previous_results.r
+++ r_scripts/previous_results.r
@@ -2,6 +2,11 @@ data <- read.table('results/summary.csv', header=T,sep=",")
 
 data$start_date.time=as.Date(as.POSIXct(data$start_date, tz = "UTC", format="%Y-%m-%dT%H:%M:%S"))
 
+data<-subset(data, date_period==2)
+data<-subset(data, buy_minimum < 8)
+data<-subset(data, buy_minimum > -8)
 
-ggplot(data=data, aes(x=start_date, y=win_lose_percentage, colour=date_period, shape = buy_minimum, group=interaction(buy_minimum, date_period))) +
-    geom_line()  + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_shape_identity()
+data <- data[complete.cases(data), ]
+
+ggplot(data=data, aes(x=start_date, y=win_lose_percentage, colour=factor(buy_minimum), group=factor(buy_minimum))) +
+    geom_line()  + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + geom_point()
