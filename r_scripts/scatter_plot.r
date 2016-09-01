9543711603486570923332ba38b6937021577670
Will Humphreys <whumphreys@gmail.com>
2016-09-01 22:12:18 +0100

    Pull out the plots to functions.

--- r_scripts/scatter_plot.r
+++ r_scripts/scatter_plot.r
@@ -5,32 +5,32 @@ data <- read.table('could_of_been_better_results/summary_high_scores.csv', heade
 
 average_per_cut_off <- aggregate(data[, 7], list(data$cut_off_percentage), mean)
 
-ggplot(data=average_per_cut_off, aes(x=Group.1, y=x, group=1)) +
+generate.average.line.plot <- function(dat, out.file) {
+
+  ggplot(data=dat, aes(x=Group.1, y=x, group=1)) +
     geom_line() +
     scale_y_continuous(breaks=seq(50,55,0.1))
-ggsave(file="could_of_been_better_results/profit_by_cutoff.png")
+  ggsave(file=out.file)
 
+}
 
-ggplot(data, aes(x=cut_off_percentage, y=winning_percentage)) +
+generate.scatter.plot <- function(dat, out.file) {
+
+  ggplot(dat, aes(x=cut_off_percentage, y=winning_percentage)) +
     geom_point(shape=1)  +    # Use hollow circles
     stat_summary(aes(y = winning_percentage,group=1), fun.y=mean, colour="red", geom="line",group=1) +
     scale_y_continuous(breaks=seq(0,100,10))
-ggsave(file="could_of_been_better_results/scatter.png")
+  ggsave(file=out.file)
+}
 
+generate.average.line.plot(average_per_cut_off, "could_of_been_better_results/profit_by_cutoff.png")
+generate.scatter.plot(data, "could_of_been_better_results/scatter.png")
 
 no_japan <- data[!grepl("usdjpy", data$data_set),]
 no_japan <- data[!grepl("eurjpy", data$data_set),]
 
 average_per_cut_off_no_japan <- aggregate(no_japan[, 7], list(no_japan$cut_off_percentage), mean)
 
-ggplot(data=average_per_cut_off_no_japan, aes(x=Group.1, y=x, group=1)) +
-    geom_line() +
-    scale_y_continuous(breaks=seq(50,55,0.1))
-ggsave(file="could_of_been_better_results/profit_by_cutoff_nojpn.png")
+generate.scatter.plot(no_japan, "could_of_been_better_results/scatter.png")
+generate.average.line.plot(average_per_cut_off_no_japan, "could_of_been_better_results/profit_by_cutoff.png")
 
-
-ggplot(no_japan, aes(x=cut_off_percentage, y=winning_percentage)) +
-    geom_point(shape=1)  +    # Use hollow circles
-    stat_summary(aes(y = winning_percentage,group=1), fun.y=mean, colour="red", geom="line",group=1) +
-    scale_y_continuous(breaks=seq(0,100,10))
-ggsave(file="could_of_been_better_results/scatter_nojpn.png")
