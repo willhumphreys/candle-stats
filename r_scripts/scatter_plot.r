464e412ac40df9478dd07d66dca5116e931ad9d2
Will Humphreys <whumphreys@gmail.com>
2016-09-04 17:23:02 +0100

    Start fixing column names

--- r_scripts/scatter_plot.r
+++ r_scripts/scatter_plot.r
@@ -30,28 +30,29 @@ generate.plots.by.date <- function(start_date, end_date, data) {
   filtered_data <- data[data$start_date == start_date,]
   filtered_data <- filtered_data[filtered_data$end_date == end_date,]
 
-  average_per_cut_off <- aggregate(filtered_data[, c("winning_percentage")], list(filtered_data$cut_off_percentage), mean)
-  average_per_cut_off_minimum <- aggregate(filtered_data[, c("winning_percentage","minimum_profit")],
-    list(filtered_data$cut_off_percentage, filtered_data$minimum_profit), mean)
+  #x cut_off_percentage y winning_percentage
+  average_per_cut_off <- setNames(aggregate(filtered_data[, c("winning_percentage")], list(filtered_data$cut_off_percentage), mean), c("cut_off_percentage", "winning_percentage"))
+  average_per_cut_off_minimum <- setNames(aggregate(filtered_data[, c("winning_percentage","minimum_profit")],
+    list(filtered_data$cut_off_percentage, filtered_data$minimum_profit), mean), c("cut_off_percentage", "minimum_profit_2", "winning_percentage", "minimum_profit" ))
 
   start_year = get.year(start_date,"_")
   end_year = get.year(end_date,"_")
 
-  ggplot(average_per_cut_off_minimum, aes(x=Group.1, y=winning_percentage)) +
+  ggplot(average_per_cut_off_minimum, aes(x=cut_off_percentage, y=winning_percentage)) +
   geom_bar(stat="identity", colour=red) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
   ggsave(file=append.year.filename("/average_cut_off_minimum_", start_year, end_year))
 
-  ggplot(average_per_cut_off_minimum, aes(x=Group.1, y=winning_percentage)) +
+  ggplot(average_per_cut_off_minimum, aes(x=cut_off_percentage, y=winning_percentage)) +
   geom_bar(stat="identity", colour="#FF9999") +
   scale_y_continuous(breaks=seq(0,80,3)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  facet_grid(. ~ Group.2)
+  facet_grid(. ~ minimum_profit)
   ggsave(file=append.year.filename("/average_cut_off_minimum_facet_", start_year,  end_year))
 
   generate.average.line.plot <- function(dat, out.file) {
 
-    ggplot(data=dat, aes(x=Group.1, y=x, group=1)) +
+    ggplot(data=dat, aes(x=cut_off_percentage, y=winning_percentage, group=1)) +
       geom_line() +
       scale_y_continuous(breaks=seq(50,80,0.3)) +
       scale_x_continuous(breaks=seq(-100,100,5))
@@ -74,7 +75,7 @@ generate.plots.by.date <- function(start_date, end_date, data) {
   generate.minimum.plots <- function(minimum_value, data) {
       filtered.data <- data[ which(data$minimum_profit==minimum_value ),]
 
-      average_per_cut_off <- aggregate(filtered.data[, c("winning_percentage")], list(filtered.data$cut_off_percentage), mean)
+      average_per_cut_off <- setNames(aggregate(filtered.data[, c("winning_percentage")], list(filtered.data$cut_off_percentage), mean), c("cut_off_percentage","winning_percentage"))
 
       generate.average.line.plot(average_per_cut_off, append.year.filename(paste("/profit_by_cutoff_", minimum_value, "_", sep=""), start_year, end_year))
       generate.scatter.plot(filtered.data, append.year.filename(paste("/scatter_", minimum_value, "_",sep=""), start_year, end_year))
