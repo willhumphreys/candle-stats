daad23da635c557659a0370783f1a89866699eef
Will Humphreys <will.humphreys@bbc.co.uk>
2016-09-14 15:51:03 +0100

    Add more tests

--- spec/trade_result_processor_spec.rb
+++ spec/trade_result_processor_spec.rb
@@ -16,6 +16,12 @@ describe 'My behaviour' do
         direction: 'long',
         profit: -6
     )
+
+    @trade_result_small = TradeResult.new(
+        timestamp: DateTime.new(2007, 12, 5),
+        direction: 'long',
+        profit: 1
+    )
   end
 
   it 'should return false when need a score of two but only score 1 ' do
@@ -26,15 +32,52 @@ describe 'My behaviour' do
     expect(trade_on).to equal(false)
   end
 
-  it 'should return true when need a score of two and score 2 ' do
+  it 'should return a winner if we have have the required_score and the next trade is above the minimum' do
 
-    trade_result_processor = TradeResultProcessor.new(10, 2)
+    window_size = 2
+    required_score = 2
+    trade_result_processor = TradeResultProcessor.new(window_size, required_score)
 
-    trade_on = trade_result_processor.process_trade_result(@trade_result, false, [], [], 2)
+    winners = []
+    losers = []
+    trade_on = trade_result_processor.process_trade_result(@trade_result, false, winners, losers, 2)
     expect(trade_on).to equal(false)
+    expect(winners.empty?).to equal(true)
+    expect(losers.empty?).to equal(true)
 
-    trade_on = trade_result_processor.process_trade_result(@trade_result, false, [], [], 2)
+    trade_on = trade_result_processor.process_trade_result(@trade_result, false, winners, losers, 2)
     expect(trade_on).to equal(true)
+    expect(winners.empty?).to equal(true)
+    expect(losers.empty?).to equal(true)
+
+    trade_on = trade_result_processor.process_trade_result(@trade_result, true, winners, losers, 2)
+    expect(trade_on).to equal(true)
+    expect(winners.size).to equal(1)
+    expect(losers.empty?).to equal(true)
+  end
+
+  it 'should not trade if the we have enough trades but the next trade is not above the minimum level' do
+
+    window_size = 2
+    required_score = 1
+    trade_result_processor = TradeResultProcessor.new(window_size, required_score)
+
+    winners = []
+    losers = []
+    trade_on = trade_result_processor.process_trade_result(@trade_result, false, winners, losers, 2)
+    expect(trade_on).to equal(true)
+    expect(winners.empty?).to equal(true)
+    expect(losers.empty?).to equal(true)
+
+    trade_on = trade_result_processor.process_trade_result(@trade_result, true, winners, losers, 2)
+    expect(trade_on).to equal(true)
+    expect(winners.empty?).to equal(true)
+    expect(losers.empty?).to equal(true)
+
+    trade_on = trade_result_processor.process_trade_result(@trade_result_small, true, winners, losers, 2)
+    expect(trade_on).to equal(true)
+    expect(winners.empty?).to equal(true)
+    expect(losers.empty?).to equal(true)
   end
 
   it 'should return true when need a score of two and score 3 ' do
