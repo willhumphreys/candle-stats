519d337543c9c62702fbb9ee56c8f551d4b4267f
Will Humphreys <whumphreys@gmail.com>
2016-09-15 19:25:58 +0100

    When we create the data frame my data turns to characters
    Need to remember to cast back to a numeric

--- r_scripts/winning_stats.r
+++ r_scripts/winning_stats.r
@@ -1,4 +1,4 @@
-years <- c('2007')
+years <- c('2007','2008','2009','2010','2011','2012','2013','2014','2015','2016')
 cut_offs <- seq(-100,100, 10)
 combinations <- expand.grid(years,cut_offs)
 
@@ -7,14 +7,17 @@ data <- read.table(file.in, header=T,sep=",")
 
 
 get.means <- function(year, cut_off, data) {
-  print(sprintf("Execute %s %s rows %f", year, cut_off, nrow(data)))
+  cut_off <- as.numeric(cut_off)
+  print(typeof(cut_off))
+  print(sprintf("Execute %s %s rows %d", year, cut_off, nrow(data)))
   datax <- data[ data$cut_off_percentage < cut_off ,]
-  print(sprintf("After cut-off %f ", nrow(datax)))
+  datax <- datax[ datax$cut_off_percentage >= cut_off - 10 ,]
+  print(sprintf("After cut-off %d ", nrow(datax)))
   datax <- datax[ datax$start_date == paste(year, '-08-02T00:00:00+00:00', sep="") ,]
-  print(sprintf("After year %f ", nrow(datax)))
+  print(sprintf("After year %d ", nrow(datax)))
   mean <- mean(datax$winning_percentage)
   row_count <- nrow(datax)
-  print(sprintf('For year %s we have a mean of %f and a count of %f cut off %s', year, mean, row_count, cut_off ))
+  print(sprintf('For year %s we have a mean of %f and a count of %d cut off %s', year, mean, row_count, cut_off ))
 }
 
 #data <- data[ which(data$data_set=='eurchf_FadeTheBreakoutNormalDaily' ),]
